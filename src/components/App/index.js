6536bc1ff83103e2bd66fbf6b1d7bb38cbd6861d
Anna <thoria@yandex.ru>
2023-08-27 10:41:20 +0300

    feat: добавить роуты в компонент App

---
+++ src/components/App/index.js
@@ -0,0 +1,20 @@
+import React, { useState, useEffect } from 'react';
+import { Routes, Route, Navigate, useNavigate } from 'react-router-dom';
+
+function App() {
+    return (
+        <div className="page">
+            <Routes>
+                <Route path="/" element={<Main />} />
+                <Route path="/sign-in" element={<Login />} />
+                <Route path="/sign-up" element={<Register />} />
+                <Route path="/movies" element={<Movies />} />
+                <Route path="/saved-movies" element={<SavedMovies />} />
+                <Route path="/profile" element={<Profile />} />
+                <Route path="*" element={<Page404 />} />
+            </Routes>
+        </div>
+    )
+}
+
+export default App;
