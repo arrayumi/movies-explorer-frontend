bd50d7fae34b967d7835e783ee9bdc9a16a4c0b2
Anna <thoria@yandex.ru>
2023-09-02 18:25:24 +0300

    feat: сверстать кнопку сохранения результатов редактирования профиля

---
+++ src/components/user/Profile/ProfileSaveButton/index.js
@@ -0,0 +1,12 @@
+import './index.css';
+
+export default function ProfileSaveButton({ errorMessage }) {
+    return (
+        <div className="profile__button-container">
+            {errorMessage ?
+                <span class="profile__error-message">{errorMessage}</span> :
+                null}
+            <button className="profile__button">Сохранить</button>
+        </div>
+    )
+}
--- src/components/user/Profile/index.js
+++ src/components/user/Profile/index.js
@@ -1,11 +1,13 @@
 import './index.css';
 import Header from '../../common/Header';
 import ProfileInput from './ProfileInput';
+import ProfileSaveButton from './ProfileSaveButton';
 
 export default function Profile() {
+    const editMode = false;
     return (
         <>
-            <Header isAuthorized={true}/>
+            <Header isAuthorized={true} />
             <main className="profile">
                 <h1 className="profile__title">Привет, Анна!</h1>
 
@@ -13,18 +15,23 @@ export default function Profile() {
                     <ProfileInput title="Имя" placeholder="Анна" />
                     <ProfileInput title="E-mail" placeholder="pochta@yandex.ru" />
                 </form>
-                <ul className="profile__links-list">
-                    <li>
-                        <button className="profile__link" type="button">
-                            Редактировать
-                        </button>
-                    </li>
-                    <li>
-                        <button className="profile__link profile__link_type_logout" type="button">
-                            Выйти из аккаунта
-                        </button>
-                    </li>
-                </ul>
+
+                {editMode ?
+                    <ProfileSaveButton errorMessage="При обновлении профиля произошла ошибка." />
+                    :
+                    <ul className="profile__links-list">
+                        <li>
+                            <button className="profile__link" type="button">
+                                Редактировать
+                            </button>
+                        </li>
+                        <li>
+                            <button className="profile__link profile__link_type_logout" type="button">
+                                Выйти из аккаунта
+                            </button>
+                        </li>
+                    </ul>
+                }
             </main>
         </>
     )
