0d1e7de1826ecdc4ec17b995da023693b7872293
Anna <thoria@yandex.ru>
2023-09-02 16:51:26 +0300

    feat: адаптивно сверстать компонент AboutMe

--- src/components/Main/AboutMe/index.js
+++ src/components/Main/AboutMe/index.js
@@ -5,12 +5,16 @@ import photo from '../../../images/about-me-photo.jpg';
 export default function AboutMe() {
     return (
         <Article sectionClassName="about-me" titleClassName="about-me__title" title="Студент" navLink="about-me">
-            <article>
+            <article className="about-me__container">
                 <img src={photo} alt="Фотография Анны" className="about-me__photo" />
-                <h3 className="about-me__name">Анна</h3>
-                <p className="about-me__caption">Фронтенд-разработчик, 29 лет</p>
-                <p className="about-me__text">Здесь будет рассказ о себе, который я еще не придумала</p>
-                <a href="3" className="about-me__link">Github</a>
+                <div className="about-me__text-container">
+                    <h3 className="about-me__name">Анна</h3>
+                    <p className="about-me__caption">Фронтенд-разработчик, 29 лет</p>
+                    <p className="about-me__text">Здесь будет рассказ о себе, который я еще не придумала</p>
+                </div>
+                <div className="about-me__link-container">
+                    <a href="3" className="about-me__link">Github</a>
+                </div>
             </article>
         </Article>
     )
