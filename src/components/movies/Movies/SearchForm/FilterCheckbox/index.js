bb94db3d1666ea549ef53ce33e7c33de6e15c662
Anna <thoria@yandex.ru>
2023-09-03 14:13:16 +0300

    hotfix: исправить ошибки валидатора в компоненте FilterCheckbox

--- src/components/movies/Movies/SearchForm/FilterCheckbox/index.js
+++ src/components/movies/Movies/SearchForm/FilterCheckbox/index.js
@@ -5,7 +5,7 @@ export default function FilterCheckbox() {
         <label className="search-form__checkbox-field">
             <input className="search-form__default-checkbox" type="checkbox"></input>
             <span className="search-form__custom-checkbox"></span>
-            <p className="search-form__checkbox-title">Короткометражки</p>
+            <span className="search-form__checkbox-title">Короткометражки</span>
         </label>
     )
 }
