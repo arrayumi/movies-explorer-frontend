44483fdda1b7eb33c7d2e7673dc81e4ff78ee0f3
Anna <thoria@yandex.ru>
2023-09-03 14:06:10 +0300

    feat: реализовать открытие/закрытие попапа навигации

--- src/components/common/NavPopup/index.js
+++ src/components/common/NavPopup/index.js
@@ -2,11 +2,16 @@ import './index.css';
 import Navigation from '../Navigation';
 
 export default function NavPopup() {
+
+    function onClick() {
+        document.querySelector('.nav-popup').classList.remove('nav-popup_active');
+    }
+
     return (
         <div className="nav-popup">
             <div className="nav-popup__container">
             <Navigation isAuthorized={true} isPopup={true}/>
-            <button className="nav-popup__close-button"></button>
+            <button className="nav-popup__close-button" type="button" onClick={onClick}></button>
             </div>
         </div>
     )
--- src/utils/constants.js
+++ src/utils/constants.js
@@ -1,4 +1,6 @@
 export const techs = ["HTML", "CSS", "JS", "React", "Git", "Express.js", "mongoDB"];
+export const navPopup = document.querySelector('.nav-popup');
+
 export const movies = [{
     image: "https://images.unsplash.com/photo-1693018365431-15b10929880b?ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D&auto=format&fit=crop&w=1887&q=80",
     nameRU: "Ночное небо",
