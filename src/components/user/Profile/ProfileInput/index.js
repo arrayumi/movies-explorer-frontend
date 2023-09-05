5dd0872371ad260b2f2bb700971575bbb5eac97b
Anna <thoria@yandex.ru>
2023-09-05 16:36:28 +0300

    feat: задать мин/макс значения инпутам компонента Profile

--- src/components/user/Profile/ProfileInput/index.js
+++ src/components/user/Profile/ProfileInput/index.js
@@ -4,7 +4,7 @@ export default function Input({title, placeholder}) {
     return (
         <label className="profile__input-container">
             <span className="profile__input-title">{title}</span>
-            <input className="profile__input" placeholder={placeholder}></input>
+            <input className="profile__input" placeholder={placeholder} minLength={2} maxLength={30}></input>
         </label>
     )
 }
