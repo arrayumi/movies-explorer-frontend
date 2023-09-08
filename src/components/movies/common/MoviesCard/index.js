e70d29c00334c6a311f61427cababa90cd34282a
Anna <thoria@yandex.ru>
2023-09-08 17:47:44 +0300

    feat: добавить отображение времени фильма в ч/мин формате

--- src/components/movies/common/MoviesCard/index.js
+++ src/components/movies/common/MoviesCard/index.js
@@ -1,8 +1,10 @@
 import './index.css';
 import MoviesCardLikeButton from './MoviesCardButton/MoviesCardLikeButton';
 import SavedMoviesCardButton from './MoviesCardButton/SavedMoviesCardButton';
+import { calculateDuration } from '../../../../utils/calculateDuration';
 
 export default function MoviesCard({ movie, isSaved, isSavedMovieCard, handleSaveMovie, handleDeleteMovie }) {
+
     return (
         <li className="movies-card">
             <article>
@@ -13,7 +15,7 @@ export default function MoviesCard({ movie, isSaved, isSavedMovieCard, handleSav
                         <SavedMoviesCardButton /> :
                         <MoviesCardLikeButton isSaved={isSaved} handleSaveMovie={handleSaveMovie} movie={movie} handleDeleteMovie={handleDeleteMovie} />}
                 </div>
-                <p className="movies-card__duration">{movie.duration}</p>
+                <p className="movies-card__duration">{calculateDuration(movie.duration)}</p>
             </article>
         </li>
     )
